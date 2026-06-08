e93a7777e22d9bb5470d855483bbeb76bca333d5
Gabriel Roberge <groberge@recursyve.io>
2026-06-07 20:10:41 -0400

    chore: update @types/node to version 24.13.1 and remove deprecated Prettier plugins from configuration
    - Updated @types/node in package.json and package-lock.json to version 24.13.1 for improved type definitions.
    - Removed deprecated Prettier plugins (css-order and tailwindcss) from the Prettier configuration file.

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,24 +2,24 @@
  * @type {import('prettier').Options}
  */
 export default {
-    "$schema": "https://json.schemastore.org/prettierrc",
-    "tabWidth": 4,
-    "singleQuote": false,
-    "trailingComma": "none",
-    "bracketSameLine": false,
-    "bracketSpacing": true,
-    "endOfLine": "lf",
-    "arrowParens": "always",
-    "plugins": ["prettier-plugin-css-order", "prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
-    "printWidth": 120,
-    "semi": true,
-    "useTabs": false,
-    "overrides": [
+    $schema: "https://json.schemastore.org/prettierrc",
+    tabWidth: 4,
+    singleQuote: false,
+    trailingComma: "none",
+    bracketSameLine: false,
+    bracketSpacing: true,
+    endOfLine: "lf",
+    arrowParens: "always",
+    plugins: ["prettier-plugin-organize-imports"],
+    printWidth: 120,
+    semi: true,
+    useTabs: false,
+    overrides: [
         {
-            "files": ["src/**/*.html"],
-            "options": {
-                "parser": "angular"
+            files: ["src/**/*.html"],
+            options: {
+                parser: "angular"
             }
         }
     ]
-}
+};
